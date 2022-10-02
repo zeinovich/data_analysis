f2bb7bafb9e4a3909434b2a5d26decb4580a4fd5
zeinovich <nikita.zinovich@gmail.com>
2022-10-02 17:34:56 +0300

    - Added more structure to the code - Added two functions instead of repeating code - Added one more cleaning step (treatment by title) - Rearranged treatments to get better result - Written resulting frame to 'netflix_clean.csv'
    I guess it is final step in treatment and cleaning. Next I will do
    analysis and visualization.

--- netflix/netflix.py
+++ netflix/netflix.py
@@ -1,4 +1,3 @@
-#%%
 import pandas as pd
 import numpy as np
 import os
@@ -8,12 +7,40 @@ os.chdir('C:/Users/nikit/data_analysis/netflix')
 with open('netflix1.csv','r', encoding='utf8') as file:
     netflix = pd.read_csv(file, index_col='show_id')
 
-# %%
-null_mask = netflix.isin(['Not Given']).any(axis=1)
-# %%
-null_frame = netflix[null_mask]
-netflix_not_null = netflix[null_mask == False]
-# %%
+def get_null(df: pd.DataFrame) -> pd.DataFrame:
+    null_mask = df.isin(['Not Given']).any(axis=1)
+    null_frame = df[null_mask]
+    df_not_null = df[null_mask == False]
+    
+    return null_frame, df_not_null
+def print_missing(df: pd.DataFrame) -> None:
+    print('Missing values:')
+    print(df.isin(['Not Given']).sum())
+null_frame, netflix_not_null = get_null(netflix)
+
+split_title = netflix['title'].str.replace(r'(\S+)\s(\S+).*', r'\1 \2', regex=True)
+split_title = pd.DataFrame(split_title, columns=['title'])
+split_title.index.name = 'show_id'
+
+# Treatment by similarities in titles
+for id in null_frame.index.tolist():
+    row = null_frame.loc[id]
+    title = row['title'].split(' ')[:2]
+    title = ' '.join(title)
+    rep_id = split_title[split_title['title'] == title]
+    if not rep_id.empty:
+        rep_id = rep_id.index[0]
+        rep_row = netflix.loc[rep_id]
+        if row['director'] == 'Not Given':
+            netflix.loc[id, 'director'] = rep_row['director']
+        if row['country'] == 'Not Given':
+            netflix.loc[id, 'country'] = rep_row['country']
+
+print_missing(netflix)
+
+null_frame, netflix_not_null = get_null(netflix)
+
+# Treatment by other similarities
 for id in null_frame.index.tolist():
     row = null_frame.loc[id]
     if row['director'] == 'Not Given':
@@ -32,9 +59,23 @@ for id in null_frame.index.tolist():
             suggest = similar['country'].value_counts().idxmax()
             netflix.loc[id, 'country'] = suggest
 
-# %%
-missing_dirs = len(netflix.query('director in ("Not Given", 0)'))
-missing_couns = len(netflix.query('country in ("Not Given", 0)'))
-# %%
-print(f'Unable to suggest: {(missing_couns) + (missing_dirs)}')
-# %%
+print(print_missing(netflix))
+
+null_frame, netflix_not_null = get_null(netflix)
+
+# Final treatment of nulls
+for id in null_frame.index.tolist():
+    row = null_frame.loc[id]
+    if row['country'] == 'Not Given':
+        similar = netflix_not_null.query('director == @row.director')
+        if not similar.empty:
+            suggest = similar['country'].value_counts().idxmax()
+            netflix.loc[id, 'country'] = suggest
+
+print_missing(netflix)
+
+# As those are useless for visualization, we can drop them
+netflix = netflix.drop(columns=['date_added', 'listed_in'], axis=1)
+
+with open('netflix_clean.csv', 'w', encoding='utf8') as file:
+    netflix.to_csv(file)
