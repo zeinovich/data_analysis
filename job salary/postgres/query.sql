38ea563235a6d9cf740aa1ac05824b6e78508cb1
zeinovich <nikita.zinovich@gmail.com>
2022-11-13 16:16:23 +0300

    First glance on data - plotted hists of all variables - tried to find best model using GridSearchCV

--- job salary/postgres/query.sql
+++ job salary/postgres/query.sql
@@ -1,24 +1,14 @@
-CREATE EXTENSION IF NOT EXISTS tablefunc;
-
-SELECT * FROM CROSSTAB($$
-	WITH us_avg AS (
-		SELECT 
-		  experience_level,
-		  work_year, 
-		  ROUND (AVG(salary_in_usd), -3) :: NUMERIC
-			AS avg_salary
-	FROM job_salary
-	GROUP BY experience_level, work_year
-	HAVING COUNT(*) > 5)	
-					   	
-	SELECT 
-		experience_level,
-		work_year,
-		avg_salary
-	FROM us_avg
-	ORDER BY experience_level ASC, work_year ASC;
-$$) as ct (experience_level VARCHAR(10),
-		  "2020" NUMERIC,
-		  "2021" NUMERIC,
-		  "2022" NUMERIC)
-ORDER BY "2020" ASC;
+DO $$                  
+    BEGIN 
+        IF NOT EXISTS
+            ( SELECT *
+			  FROM job_salary
+			  WHERE salary = 100000
+            )
+        THEN
+            INSERT INTO job_salary (work_year, salary)
+			VALUES (2020, 100000);
+		END IF;
+        END IF ;
+    END
+   $$ ;
